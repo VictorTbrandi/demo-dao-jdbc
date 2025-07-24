23d2ec942ac19c91871b4f9bf7f57dc5baec2d79
Victor Brandi <victorterrenguibrand@gmail.com>
2025-07-23 23:14:03 -0300

    Inserte implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -27,5 +27,10 @@ public class Program {
         for (Seller s : sellers) {
             System.out.println(s);
         }
+
+        System.out.println("=== TESTE 4: seller insert ===");
+        Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id= " + newSeller.getId());
     }
 }
